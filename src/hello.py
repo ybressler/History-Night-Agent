65498cd6749ecc426f57376db723be3965642c8e
Yaakov Bressler <yaakovbressler@gmail.com>
2025-05-25 15:33:26 -0400

    updating to actually use gemini

--- src/hello.py
+++ src/hello.py
@@ -1,5 +1,21 @@
+from pydantic_ai import Agent
+from pydantic_ai.models.gemini import GeminiModel
+
+
+class HistoryNightAgent:
+    model = GeminiModel("gemini-2.5-pro-preview-05-06")
+    agent = Agent(
+        model,
+        system_prompt="Be concise, reply with one sentence.",
+    )
+
+
 def main():
-    print("Hello from history-night-agent!")
+    hn_agent = HistoryNightAgent()
+    result = hn_agent.run_sync(
+        "What is the earliest year glass windows were used in a palace?"
+    )
+    print(result.output)
 
 
 if __name__ == "__main__":
