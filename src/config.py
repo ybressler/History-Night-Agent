a7048b60802bb5b5a05d3673eac581cf0583ea26
Yaakov Bressler <yaakovbressler@gmail.com>
2025-05-25 15:43:26 -0400

    Got the model to start working

---
+++ src/config.py
@@ -0,0 +1,11 @@
+"""
+Loads secrets from environment variables and more.
+"""
+
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
--- src/hello.py
+++ src/hello.py
@@ -1,9 +1,16 @@
 from pydantic_ai import Agent
 from pydantic_ai.models.gemini import GeminiModel
+from pydantic_ai.providers.google_gla import GoogleGLAProvider
+
+from src.config import GEMINI_API_KEY
 
 
 class HistoryNightAgent:
-    model = GeminiModel("gemini-2.5-pro-preview-05-06")
+    model = GeminiModel(
+        model_name="gemini-2.5-flash-preview-05-20",
+        provider=GoogleGLAProvider(api_key=GEMINI_API_KEY),
+    )
+
     agent = Agent(
         model,
         system_prompt="Be concise, reply with one sentence.",
@@ -11,9 +18,10 @@ class HistoryNightAgent:
 
 
 def main():
-    hn_agent = HistoryNightAgent()
+    hn_agent = HistoryNightAgent().agent
     result = hn_agent.run_sync(
-        "What is the earliest year glass windows were used in a palace?"
+        "What is the earliest year glass windows were used in a palace? (Starting in the year 850 AD)",
+        output_type=int,
     )
     print(result.output)
 
